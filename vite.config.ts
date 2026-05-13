cdc8f889956b65dfb364aa7d712c596d53c0af25
Seengar <seengar237@gmail.com>
2026-05-14 04:42:36 +0500

    chore: rebuild 2026-05-13T23:42:36.179Z

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// build: 2026-05-13T23:42:36.179Z
 export default defineConfig({
   plugins: [react()],
   server: { host: true }
